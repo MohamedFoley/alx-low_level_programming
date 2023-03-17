54b1c8970e56bca43cf6028dd16f6922f7a0af41
MohamedFoley <mohamedwfoley@gmail.com>
2023-03-16 19:17:45 -0700

    added Description to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - this is the main function
- *Return: always 0
+ * main - Entry point
+ *
+ * Description: a progam that prints "the project sentence"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
